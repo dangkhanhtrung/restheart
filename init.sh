639a5905e4e37a07ab12f7750169fc895dd9c61d
binhthgc <binhth@fds.vn>
2019-10-03 17:01:00 +0700

    comit init

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ cd gateway && npm install
 cd ../oauth2-client-prod && npm install
 cd ../restheart-security && npm install
 cd ../vuejx-client-prod && npm install
-cd ../vuejx-core-prod && npm install
+cd ../vuejx-core-prod && npm run clean
 cd ../restheart-core && mvn package
 docker volume create --name=mongo_data
 docker-compose up
